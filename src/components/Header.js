6968741009e9efce0cd854bdece2dba5c61b6ea6
Yati R&D Lab <yatishbathla@gmail.com>
2026-02-26 15:58:58 +0530

    remove redudant code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Header.css';
-import { Link } from "react-router-dom";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
